a88d960fed3dcd948167b91c1a64ffcafe6e689b
PyGamer0 <64531844+PyGamer0@users.noreply.github.com>
2021-09-03 12:21:52 +0530

    add batch; its faster
    thanks @benmoran56

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import pyglet
 
 GRAVITY = Vec2(0, -9.8)
 
+
 class Point:
-    def __init__(self, position, is_locked=False):
+    def __init__(self, position, batch, is_locked=False):
         self.position = position
         self.is_locked = is_locked
 
         self._previous_position = position
-    
+
+        self.shape = pyglet.shapes.Circle(position.x, position.y, 5, color=(230, 230, 240), batch=batch)
+
     def update(self, dt):
         if self.is_locked:
             return
@@ -18,16 +21,17 @@ class Point:
         self.position = self.position + (self.position - self._previous_position)
         self.position = self.position + (GRAVITY.scale(dt ** 2))
         self._previous_position = position_before_update
-    
-    def draw(self):
-        shape = pyglet.shapes.Circle(self.position.x, self.position.y, 5)
-        shape.color = (230, 230, 240)
-        shape.draw()
+        self.shape.position = self.position
+
 
 class Stick:
-    def __init__(self, pos_a, pos_b, length=None):
+    def __init__(self, pos_a, pos_b, batch, length=None):
         self.pos_a = pos_a
         self.pos_b = pos_b
+
+        self.line = pyglet.shapes.Line(self.pos_a.position.x, self.pos_a.position.y,
+                                       self.pos_b.position.x, self.pos_b.position.y,
+                                       color=(230, 230, 240), batch=batch)
 
         if length is not None:
             self.length = length
@@ -44,28 +48,27 @@ class Stick:
         if not self.pos_b.is_locked:
             self.pos_b.position = center - direction.scale(self.length / 2)
 
-    def draw(self):
-        shape = pyglet.shapes.Line(self.pos_a.position.x, self.pos_a.position.y,
-                self.pos_b.position.x, self.pos_b.position.y)
-        shape.color = (230, 230, 240)
-        shape.draw()
+        self.line.position = *self.pos_a.position, *self.pos_b.position
+
 
 class Window(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.points = [Point(Vec2(100, 400)),
-                Point(Vec2(150, 400)),
-                Point(Vec2(200, 400)),
-                Point(Vec2(250, 400)),
-                Point(Vec2(350, 400)),
-                Point(Vec2(400, 400), True)]
-        self.sticks = [Stick(self.points[0], self.points[1]),
-                Stick(self.points[1], self.points[2]),
-                Stick(self.points[2], self.points[3]),
-                Stick(self.points[3], self.points[4]),
-                Stick(self.points[4], self.points[5]),
-                ]
+        self.batch = pyglet.graphics.Batch()
+
+        self.points = [Point(Vec2(100, 400), self.batch),
+                       Point(Vec2(150, 400), self.batch),
+                       Point(Vec2(200, 400), self.batch),
+                       Point(Vec2(250, 400), self.batch),
+                       Point(Vec2(350, 400), self.batch),
+                       Point(Vec2(400, 400), self.batch, True)]
+        self.sticks = [Stick(self.points[0], self.points[1], self.batch),
+                       Stick(self.points[1], self.points[2], self.batch),
+                       Stick(self.points[2], self.points[3], self.batch),
+                       Stick(self.points[3], self.points[4], self.batch),
+                       Stick(self.points[4], self.points[5], self.batch),
+                       ]
         self.paused = False
 
     def update(self, dt):
@@ -81,23 +84,18 @@ class Window(pyglet.window.Window):
 
     def on_draw(self):
         self.clear()
-
-        for stick in self.sticks:
-            stick.draw()
-
-        for point in self.points:
-            point.draw()
+        self.batch.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
         if button == pyglet.window.mouse.LEFT:
-            self.points.append(Point(Vec2(x, y)))
+            self.points.append(Point(Vec2(x, y), self.batch))
 
     def on_key_press(self, symbol, modifiers):
         if symbol == pyglet.window.key.SPACE:
             self.paused = not self.paused
 
+
 win = Window(800, 800)
 
 pyglet.clock.schedule_interval(win.update, 1 / 60)
 pyglet.app.run()
-
